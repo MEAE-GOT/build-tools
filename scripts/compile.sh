43e450e300a6070578b2e94b73710aa598424c3a
thomas <thomas.johansson@melcogot.com>
2017-02-02 09:24:47 +0000

    Separate build script and test run script

---
+++ scripts/compile.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+### Force script to be run as a bash script and not a sh script
+if [ "$(ps -p "$$" -o comm=)" != "bash" ]; then
+    # Taken from http://unix-linux.questionfor.info/q_unix-linux-programming_85038.html
+    bash "$0" "$@"
+    exit "$?"
+fi
+
+### make the script fail if any command fails
+set -e
+
+### arguments
+workspace="$1"
+qmake="$2"
+cd $workspace
+
+### qmake path
+
+#qmake=/home/vagrant/Qt-5.6/5.6/gcc_64/bin/qmake
+
+### java tool to filter out Qt projects and sort them by dependency
+projects=$(java -jar ../build-tools/ProjectSorter/deploy/ProjectSorter.jar . .pro)
+
+echo "Compiling..."
+
+for proj in ${projects[@]}; do
+
+    projName=${proj%/*}
+
+    echo "***** Compile $projName *****"
+    cd $projName
+
+    make clean
+    $qmake
+    make
+
+    cd -
+
+    ### compile test code if any
+    testDir=$projName/tests/auto/test
+    if [ -d $testDir ]; then
+        # compile tests code
+        echo "***** Compiling test code for $projName *****"
+        cd $proj/tests
+        make clean
+    	$qmake
+        make
+	cd -
+    fi
+
+done
+
+echo ""
+echo "All source code compiled successfuly!"
---
+++ scripts/run_gtests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+### Force script to be run as a bash script and not a sh script
+if [ "$(ps -p "$$" -o comm=)" != "bash" ]; then
+    # Taken from http://unix-linux.questionfor.info/q_unix-linux-programming_85038.html
+    bash "$0" "$@"
+    exit "$?"
+fi
+
+### arguments
+workspace="$1"
+
+cd $workspace
+
+### java tool to filter out Qt projects and sort them by dependency
+projects=$(java -jar ../build-tools/ProjectSorter/deploy/ProjectSorter.jar . .pro)
+
+echo "running tests..."
+
+for proj in ${projects[@]}; do
+
+    projName=${proj%/*}
+
+    echo "***** Running unit test(s) for $projName *****"
+    testDir=$projName/tests/auto/test
+    if [ -d $testDir ]; then
+        cd $proj/tests
+        
+        auto/test/test --gtest_output=xml
+        cd -
+
+        mv $projName/tests/test_detail.xml test-results/$projName-test_detail.xml
+    else
+        echo "No tests found!"
+    fi
+
+done
+
+echo ""
+echo "Done running tests!"
