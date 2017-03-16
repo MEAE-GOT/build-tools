ece8a9be19a20ffea81d1dbff1853542b1e58a79
thomas68 <thomas.johansson@live.com>
2017-03-16 14:01:50 +0100

    adjust test script to match test client

--- scripts/test_w3cserver_rpi3.sh
+++ scripts/test_w3cserver_rpi3.sh
@@ -3,15 +3,17 @@
 set -e
 
 server=$1
-test_case=$2
-build_number=$3
-repo_revision=$4
+no_clients=$2
+test_case=$3
+build_number=$4
+repo_revision=$5
 
 echo "Test client parameters:"
-echo "  server host: $server"
-echo "    test case: $test_case"
-echo " build number: $build_number"
-echo "repo revision: $repo_revision"
+echo "      server host: $server"
+echo "number of clients: $no_clients"
+echo "        test case: $test_case"
+echo "     build number: $build_number"
+echo "   repo revision: $repo_revision"
 
 ssh_key="/media/pfpro/SpareDisc/bamboo/ssh/id_rsa"
 w3cserver_build_path="../LP-W3CSER-JOB1/gdp/gdp-src-build/tmp/work/cortexa7hf-neon-vfpv4-poky-linux-gnueabi/w3c-server/1.0-r0/build/src"
@@ -34,7 +36,7 @@ sleep 10
 echo "Server started"
 
 echo "Start test clients..."
-$test_client wss://$server:8080 $test_case
+$test_client -url wss://$server:8080 -c $no_clients $test_case
 
 echo "Closing ssh connection."
 kill $pid
