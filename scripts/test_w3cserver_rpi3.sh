38f717d07e2f7c24d7753c34ffbb4c96d9dd170d
thomas68 <thomas.johansson@live.com>
2017-03-22 14:19:36 +0100

    removed installation code from test script, moved to install_package

--- scripts/test_w3cserver_rpi3.sh
+++ scripts/test_w3cserver_rpi3.sh
@@ -14,18 +14,14 @@ echo "     build number: $build_number"
 echo "    repo revision: $repo_revision"
 
 ssh_key="/media/pfpro/SpareDisc/bamboo/ssh/id_rsa"
-w3cserver_build_path="gdp/gdp-src-build/tmp/work/cortexa7hf-neon-vfpv4-poky-linux-gnueabi/w3c-server/1.0-r0/build/src"
 test_client="../LP-W3CSER-COM/w3c-server/W3CQtTestClient/src/W3CQtTestClient"
 
-echo "Stopping and removing old w3cserver from client..."
+echo "Kill W3CServer if any running..."
 ssh -i $ssh_key root@$server 'killall -9 W3CServer'
 
 set -e
 
-echo "Copying new w3cserver..."
-scp -i $ssh_key "${w3cserver_build_path}/W3CServer" root@$server:/usr/bin
-
-echo "Start new server and whait a while..."
+echo "Start W3CServer and whait a while..."
 (ssh -i $ssh_key root@$server 'nohup /usr/bin/W3CServer -secure')&
 
 pid=$!
