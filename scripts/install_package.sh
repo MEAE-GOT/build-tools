d9ee53f41d8d7b78e0f0476c984f179499e19f44
thomas68 <thomas.johansson@live.com>
2017-03-22 14:12:06 +0100

    add script to install package on target

---
+++ scripts/install_package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+host=$1
+package_file=$2
+
+process_to_stop="W3CServer"
+ssh_key="/media/pfpro/SpareDisc/bamboo/ssh/id_rsa"
+package_name=$(basename $package_file)
+
+echo "Stopping $process_to_stop ..."
+ssh -i $ssh_key root@$host "killall -9 $process_to_stop"
+
+set -e
+
+echo "Copy $package_file to target ..."
+scp -i $ssh_key "$package_file" root@$host:/home/root
+
+echo "Install $package_name ..."
+ssh -i $ssh_key root@$host "rpm -i --replacepkgs /home/root/$package_name"
+
+echo "done!"
