1a666cee2340dc1527a2898d063d82647849159d
thomas68 <thomas.johansson@live.com>
2017-05-04 11:15:18 +0200

    remove old config when installing package

--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -11,6 +11,10 @@ package_name=$(basename $package_file)
 echo "Stopping $process_name ..."
 ssh -i $ssh_key root@$host "killall -9 $process_name"
 
+# remove old config
+ssh -i $ssh_key root@$host "rm /home/root/.config/MelcoGOT/$process_name.ini"
+ssh -i $ssh_key root@$host "rm /.config/MelcoGOT/$process_name.ini"
+
 ## stop and fail script on errors
 set -e
 
